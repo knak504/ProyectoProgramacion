82d0d96efd07e0f176032f9e00b4ab1bf87141b7
Martha Haydé Benítez <adelheid.bg@gmail.com>
2016-03-08 23:10:52 -0600

    Update DetallePersona.java

--- app/src/main/java/com/proyecto/programacion/proyectoprogramacion/DetallePersona.java
+++ app/src/main/java/com/proyecto/programacion/proyectoprogramacion/DetallePersona.java
@@ -120,11 +120,12 @@ public class DetallePersona extends AppCompatActivity {
         });
         
         
-            Facebook_Link.setOnClickListener(new View.OnClickListener() {
+          Facebook_Link.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(getString(item.getFacebook_Link()))));
             }
+        });
         
         Twitter_Link.setOnClickListener(new View.OnClickListener() {
             @Override
