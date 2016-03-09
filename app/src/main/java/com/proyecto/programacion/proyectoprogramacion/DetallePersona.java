4bef300e326a6b28416d633207893ee2d7b6fb7d
edyoliva <edy.oliva@ujcv.edu.hn>
2016-03-08 22:44:07 -0600

    Update DetallePersona.java

--- app/src/main/java/com/proyecto/programacion/proyectoprogramacion/DetallePersona.java
+++ app/src/main/java/com/proyecto/programacion/proyectoprogramacion/DetallePersona.java
@@ -119,6 +119,13 @@ public class DetallePersona extends AppCompatActivity {
 
         });
         
+        
+            Facebook_Link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(getString(item.getFacebook_Link()))));
+            }
+        
         Twitter_Link.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
